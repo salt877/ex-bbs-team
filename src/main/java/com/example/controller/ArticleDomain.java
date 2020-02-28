a4ba1e5b7f0783037a75fb771266e8a6cecfd16e
adachisota <sota.adachi@rakus-partners.co.jp>
2020-02-28 12:10:48 +0900

    新規ドメインクラスを作成

---
+++ src/main/java/com/example/controller/ArticleDomain.java
@@ -0,0 +1,67 @@
+package com.example.controller;
+
+import java.util.List;
+
+/**
+ * 投稿情報を表すドメイン.
+ * 
+ * @author sota_adachi
+ *
+ */
+public class ArticleDomain {
+	/**
+	 * 記事ID
+	 */
+	private Integer id;
+	/**
+	 * 投稿者名
+	 */
+	private String name;
+	/**
+	 * 記事
+	 */
+	private String content;
+	/**
+	 * コメントリスト
+	 */
+	private List<CommentDomain> commentList;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+
+	}
+	
+	public List<CommentDomain> getCommentList() {
+		return commentList;
+	}
+	
+	public void setCommentList(List<CommentDomain> commentList) {
+		this.commentList = commentList;
+	}
+
+	@Override
+	public String toString() {
+		return "ArticleDomain [id=" + id + ", name=" + name + ", content=" + content + ", commentList=" + commentList
+				+ "]";
+	}
+}
