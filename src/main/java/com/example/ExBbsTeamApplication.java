efa28fd113b80bd5f38d7b990f75e7d20f7d2c15
shiodarina <rina.shioda@rakus-partners.co.jp>
2020-02-28 11:50:30 +0900

    first commit

---
+++ src/main/java/com/example/ExBbsTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBbsTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBbsTeamApplication.class, args);
+	}
+
+}
